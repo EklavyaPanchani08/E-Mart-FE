aa11e68fe67ad96e1055c10970fdd3aaaf15d519
Eklavya <eklavyapanchani99792@gmail.com>
2022-08-18 17:27:22 +0530

    change from PC

--- src/Router/router.js
+++ src/Router/router.js
@@ -1,23 +1,16 @@
 import React from 'react'
-import {useRoutes } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 import About from '../components/About';
 import Contact from '../components/Contact';
 import Home from '../components/Home';
 import Login from '../components/Login';
 import Product from '../components/Product';
 import Products from '../components/Products';
-import Regiter from '../components/Regiter';
+import Register from '../components/Register';
 import Test from '../components/Test';
-import React from "react";
-import { useRoutes } from "react-router-dom";
-import Home from "../components/Home";
-import Login from "../components/Login";
-import Product from "../components/Product";
-import Products from "../components/Products";
-import Register from "../components/Register";
-import Test from "../components/Test";
 
 const Router = () => {
+
   let element = useRoutes([
     {
       path: "/",
@@ -44,52 +37,19 @@ const Router = () => {
       isExact: true,
       element: <Register />,
     },
+    {
+      path: "/about",
+      isExact: true,
+      element: <About />,
+    },
+    {
+      path: "/contact",
+      isExact: true,
+      element: <Contact />,
+    },
   ]);
 
-<<<<<<< HEAD
-    let element = useRoutes([
-      {
-        path: "/",
-        isExact: true,
-        element: <Home />,
-      },
-      {
-        path: "/products",
-        isExact: true,
-        element: <Products />,
-      },
-      {
-        path: "/products/:id",
-        isExact: true,
-        element: <Product />,
-      },
-      {
-        path: "/login",
-        isExact: true,
-        element: <Login />,
-      },
-      {
-        path: "/regiter",
-        isExact: true,
-        element: <Regiter />,
-      },
-      {
-        path: "/about",
-        isExact: true,
-        element: <About />,
-      },
-      {
-        path: "/contact",
-        isExact: true,
-        element: <Contact />,
-      },
-    ]);
-  
-    return element;
-}
-=======
   return element;
-};
->>>>>>> 9b99ab97b11e2f8752b3f0a6e27e21e5ad88f74d
+}
 
 export default Router;
