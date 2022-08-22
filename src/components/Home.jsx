a61f98980f333e04bc29403f3d3813cce49ab644
Eklavya_Panchani <104212977+EklavyaPanchani08@users.noreply.github.com>
2022-08-22 08:01:57 +0530

    change from leptop

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,103 +1,130 @@
 import React from "react";
 import banner1 from "../assets/banner1.jpeg";
 import banner2 from "../assets/banner2.jpeg";
-import banner3 from "../assets/banner3.jpeg";
-import banner4 from "../assets/banner4.jpeg";
 import banner5 from "../assets/banner5.jpeg";
 import imgForTest from "../assets/testHome1.jpeg";
-import { Carousel, Card, Image } from 'antd';
-import '../scss/home.scss'
+import { Carousel, Card, Image } from "antd";
+import "../scss/home.scss";
 import Meta from "antd/lib/card/Meta";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-
-
   return (
     <div>
-      <Carousel autoplay>
-        <div>
-          <img className="slider" src={banner2} alt="background" />
+      <div
+        id="carouselExampleIndicators"
+        class="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
         </div>
-        <div>
-          <img className="slider" src={banner1} alt="background" />
-        </div>
-        <div>
-          <img className="slider" src={banner3} alt="background" />
-        </div>
-        <div>
-          <img className="slider" src={banner4} alt="background" />
-        </div>
-
-        <div>
-          <img className="slider" src={banner5} alt="background" />
-        </div>
-      </Carousel>
-
-      <div className="d-flex mx-auto">
-        <div className="card h-100 text-center p-4">
-          <img
-            src={imgForTest}
-            className="card-img-top"
-            height="250px"
-            width="250px"
-          />
-          <div className="card-body">
-            <h5 className="card-title">
-              Hii fjkehkrhfuoh cxiubvdsoigdhicoxn dsiufhieuahuxkvnjcx
-            </h5>
-            <p className="card-text lead fw-bold">$500</p>
-            <Link
-              to={"/products/"}
-              className="btn btn-outline-dark"
-            >
-              Buy Now
-            </Link>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={banner1} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={banner2} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={banner5} class="d-block w-100" alt="..." />
           </div>
         </div>
-        <div className="card h-100 text-center p-4">
-          <img
-            src={imgForTest}
-            className="card-img-top"
-            height="250px"
-            width="250px"
-          />
-          <div className="card-body">
-            <h5 className="card-title">
-              Hii fjkehkrhfuoh cxiubvdsoigdhicoxn dsiufhieuahuxkvnjcx
-            </h5>
-            <p className="card-text lead fw-bold">$500</p>
-            <Link
-              to={"/products/"}
-              className="btn btn-outline-dark"
-            >
-              Buy Now
-            </Link>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      <div className="container">
+        <div className="d-flex mx-auto">
+          <div className="card h-100 text-center p-4">
+            <img
+              src={imgForTest}
+              className="card-img-top"
+              height="250px"
+              width="250px"
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Hii fjkehkrhfuoh cxiubvdsoigdhicoxn dsiufhieuahuxkvnjcx
+              </h5>
+              <p className="card-text lead fw-bold">$500</p>
+              <Link to={"/products/"} className="btn btn-outline-dark">
+                Buy Now
+              </Link>
+            </div>
           </div>
-        </div>
-        <div className="card h-100 text-center p-4">
-          <img
-            src={imgForTest}
-            className="card-img-top"
-            height="250px"
-            width="250px"
-          />
-          <div className="card-body">
-            <h5 className="card-title">
-              Hii fjkehkrhfuoh cxiubvdsoigdhicoxn dsiufhieuahuxkvnjcx
-            </h5>
-            <p className="card-text lead fw-bold">$500</p>
-            <Link
-              to={"/products/"}
-              className="btn btn-outline-dark"
-            >
-              Buy Now
-            </Link>
+          <div className="card h-100 text-center p-4">
+            <img
+              src={imgForTest}
+              className="card-img-top"
+              height="250px"
+              width="250px"
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Hii fjkehkrhfuoh cxiubvdsoigdhicoxn dsiufhieuahuxkvnjcx
+              </h5>
+              <p className="card-text lead fw-bold">$500</p>
+              <Link to={"/products/"} className="btn btn-outline-dark">
+                Buy Now
+              </Link>
+            </div>
+          </div>
+          <div className="card h-100 text-center p-4">
+            <img
+              src={imgForTest}
+              className="card-img-top"
+              height="250px"
+              width="250px"
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Hii fjkehkrhfuoh cxiubvdsoigdhicoxn dsiufhieuahuxkvnjcx
+              </h5>
+              <p className="card-text lead fw-bold">$500</p>
+              <Link to={"/products/"} className="btn btn-outline-dark">
+                Buy Now
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-
-    </div >
+    </div>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,37 +3,44 @@ import { Link } from "react-router-dom";
 import logo from "../assets/logo-black.png"
 
 const Navbar = () => {
+
+  // (document).ready(function () {
+  //   ('.nav-toggle').click(function () {
+  //       ('navbar-collapse').slideToggle()
+  //   });
+  // }); 
+
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light py-2 bg-white shadow-sm">
+      <nav className="navbar navbar-expand-md navbar-light bg-light py-2 bg-white shadow-sm">
         <div className="container">
           <Link className="navbar-brand fw-4 fs-4" to="/">
             <img src={logo} alt="logo" width='170px' />
           </Link>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li className="nav-item px-2">
+            <ul className="navbar-nav mx-auto">
+              <li className="nav-item px-lg-2">
                 <Link className="nav-link active" aria-current="page" to="/">
                   Home
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/products">
+              <li className="nav-item px-lg-2">
+                <Link className="nav-link active" to="/products">
                   Product
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">
+              <li className="nav-item px-lg-2">
+                <Link className="nav-link active" to="/about">
                   About
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/contact">
+              <li className="nav-item px-lg-2">
+                <Link className="nav-link active" to="/contact">
                   Contact
                 </Link>
               </li>
             </ul>
-            <div className="button">
+            <div>
               <Link to="/login" className="btn btn-outline-dark">
                 Login
               </Link>
@@ -45,6 +52,9 @@ const Navbar = () => {
               </Link>
             </div>
           </div>
+      <div className="nav-toggle">
+        <a  href="#" className="text-dark manu-toggle"><i className="fa-solid fa-align-right"></i></a>
+      </div>
         </div>
       </nav>
     </div>
